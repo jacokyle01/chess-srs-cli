bb3b49f36e348840e0f292c2a9d0fcaf6353d932
Kyle Jacobson <jacokyle01@gmail.com>
2024-02-17 11:00:06 -0600

    handle next by printing current position from path

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 const readline = require("readline");
 const ChessSrs = require("./chess-srs/dist/main");
+const { Chess } = require("chess.js");
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
 
-// Ask the user for their name
-rl.question("What is your name? ", (name) => {
-	// Greet the user
-	console.log(`Hello, ${name}!`);
-	const chessSrs = ChessSrs({ buckets: [1, 10, 100] });
-	console.log(chessSrs.state());
+// 1. e4 e5 2. f4 exf4 (2... d5) 3. Nf3 (3. Bc4 Qh4+ (3... d5) 4. Kf1) (3. d4 Qh4+ 4. Ke2) 3... d6 { test } (3... g5) *
 
-	// Close the readline interface
-	rl.close();
+const chessSrs = ChessSrs({ buckets: [1, 10, 100] });
+rl.question("Enter a repertoire to start\n", (pgn) => {
+	// console.log(pgn);
+	chessSrs.addSubrepertoires(pgn, "white");
+	// rl.close();
+	rl.question("Select which repertoire to train. Ex. 0\n", (num) => {
+		// console.log(num);
+		chessSrs.load(0);
+		console.log("Enter commands to train your repertoire. Press h for help.\n");
+		rl.on("line", (char) => {
+			switch (char) {
+				case "h":
+					console.log("test");
+					break;
+				case "n": //add async function here to handle training
+					chessSrs.next();
+					console.log(chessSrs.path());
+					// console.log(node.data.san);
+					printBoard(chessSrs.path());
+			}
+		});
+	});
 });
 
-// Listen for the close event to exit the program
+// // Listen for the close event to exit the program
 rl.on("close", () => {
 	console.log("Goodbye!");
 	process.exit(0);
 });
+
+const printBoard = (path) => {
+	const chess = new Chess();
+	path.forEach((node) => chess.move(node.data.san));
+	console.log(chess.ascii());
+};
